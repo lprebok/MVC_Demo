62eede893047ac0a2ad8c61bae21707fbd1a4a49
Hero <Hero@DESKTOP-DITITEO>
2018-11-26 22:33:07 +0800

    新增student类

---
+++ MVC_Demo/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Demo.Models
+{
+    public class Student
+    {
+        public int Code { get; set; }
+    }
+}
